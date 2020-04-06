8d052f648e8b09c658923fc4c8f47a42c5b6891b
franjpr <franpr90@gmail.com>
2020-04-06 23:19:51 +0100

    add ssh travis test

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 HOST="ec2-52-212-86-43.eu-west-1.compute.amazonaws.com"
 USER="ec2-user"
-KEY="./key-franjpr.pem"
+KEY=$1
 
 ssh -i $KEY $USER@$HOST \
   "sudo yum install docker -y && sudo service docker start && docker kill $(docker ps -q) && sudo docker pull franjpr/my-node-server && sudo docker run -d -p 80:8080 franjpr/my-node-server"
