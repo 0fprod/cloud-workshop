6a5623fc11d5ac22215f106821c0452fb235d53e
franjpr <franpr90@gmail.com>
2020-04-06 23:10:46 +0100

    travis deploy test

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ USER="ec2-user"
 KEY="./key-franjpr.pem"
 
 ssh -i $KEY $USER@$HOST \
-  "sudo yum install docker -y && sudo service docker start && sudo docker run -d -p 80:8080 franjpr/my-node-server"
+  "sudo yum install docker -y && sudo service docker start && docker kill $(docker ps -q) && sudo docker pull franjpr/my-node-server && sudo docker run -d -p 80:8080 franjpr/my-node-server"
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Deploy test!');
+  res.send('La prueba de miwichi');
 });
 
 app.listen(PORT, HOST);
