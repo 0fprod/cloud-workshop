3cccb7229fa288c85a577b67a44398efcd434768
franjpr <franpr90@gmail.com>
2020-04-06 22:25:15 +0100

    added travis

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ USER="ec2-user"
 KEY="./key-franjpr.pem"
 
 ssh -i $KEY $USER@$HOST \
-  "sudo yum install docker -y && sudo service docker start && sudo docker run -d -p 80:8080 --name franjpr franjpr/my-node-server"
+  "sudo yum install docker -y && sudo service docker start && sudo docker run -d -p 80:8080 franjpr/my-node-server"
